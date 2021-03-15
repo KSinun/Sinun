19550f982f4776ccaf22ce9d47f8a62142b377e9
DESKTOP-NUOPUIF\ITPS <kswon1997@gmail.com>
2021-03-15 17:27:08 +0900

    최초

---
+++ src/chapter1/Ex1.java
@@ -0,0 +1,20 @@
+package chapter1;
+// 클래스 : 소스 코드의 최소 단위
+public class Ex1 {
+		// 메인 함수 : 시작점, 내가 작성한 소스코드의 시작이 어딘지 알려주는 기능
+	public static void main(String[] args) {
+		// 한 줄 주석 개발자참고용
+		
+//		 무언가를 출력하기위한 기능
+//		 (   ) 안에다가 출력 대상을 지정
+//		System.out .println("Hello world~!");
+		System.out.println("주석 연습 중...");
+		
+		System.out.println("이거좋네");
+		System.out.println("eee");
+		
+		
+		
+	}
+}
+
---
+++ src/chapter1/Ex2.java
@@ -0,0 +1,45 @@
+package chapter1;
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+//		문자열 - 문자의 나열
+//		문자열은 꼭 쌍따옴표를 사용해서 표현해야한다.
+//		System.out.println("Hello World~!");
+		
+		
+//		System.out.println("I am");
+//		System.out.println("a");
+//		System.out.println("Java Programmer");
+		
+		
+		
+//		System.out.println("오늘은");
+//		System.out.println("자바 수업");
+//		System.out.println("첫 번째 날입니다");
+		
+//		
+//		System.out.println("오늘은");
+//		System.out.println("내가");
+//		System.out.println("짜파게티 요리사");
+		
+//		System.out.println(1);
+//		System.out.println(3.14);
+//		System.out.println("a");
+//		System.out.println("asdf");
+//		
+		
+		
+		System.out.println("오늘의 기온 : " + -5.3 + "C");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
