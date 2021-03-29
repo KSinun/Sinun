57f662ce56391eab78b0d2ab70d4ce0b780bbd9e
DESKTOP-NUOPUIF\ITPS <kswon1997@gmail.com>
2021-03-29 17:49:50 +0900

    no message

---
+++ src/chapter13/Ex2.java
@@ -0,0 +1,11 @@
+package chapter13;
+
+public class Ex2 {
+	public static void main(String[] args) {
+		System.out.println(인터페이스명.memVal1);
+		System.out.println(인터페이스명.memVal2);
+		System.out.println(인터페이스명.memVal3);
+		System.out.println(인터페이스명.memVal4);
+	}
+
+}
